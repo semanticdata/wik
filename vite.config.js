837efdfefaaef57a4783a6a32fd4a0e52ccdb11d
semanticdata <contact@miguelpimentel.do>
2024-09-12 12:05:04 -0500

    add page.html and test.html as entry points

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
-export default {
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
   base: "/wik/",
-};
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        page: resolve(__dirname, "page.html"),
+        test: resolve(__dirname, "test.html"),
+      },
+    },
+  },
+});
